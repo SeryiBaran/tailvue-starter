a26ff0bfc0d82c6fa270eab31aa6b6c81f4c4890
SeryiBaran <seryibaranminepy@yandex.ru>
2023-04-04 11:03:47 +0300

    minor edits

--- src/App.vue
+++ src/App.vue
@@ -8,30 +8,28 @@ const localeStore = useLocalStorage('locale', i18n.locale)
 </script>
 
 <template>
-  <div class="mx-auto flex min-h-[100vh] max-w-5xl flex-col p-4 text-center font-sans">
-    <div class="m-auto">
-      <header class="flex flex-wrap items-center justify-center gap-4">
-        <h1>
-          Tailvue
-        </h1>
+  <div class="m-auto flex min-h-[100vh] flex-col items-center justify-center p-4 text-center font-sans">
+    <header class="flex flex-wrap items-center justify-center gap-4">
+      <h1>
+        Tailvue
+      </h1>
 
-        <select v-model="localeStore" class="text-gray-800">
-          <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
-            {{ locale }}
-          </option>
-        </select>
-      </header>
-      <main class="flex flex-col items-center gap-4">
-        <section>
-          <p>
-            {{ $t("about") }}
-          </p>
-        </section>
+      <select v-model="localeStore" class="text-gray-800">
+        <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
+          {{ locale }}
+        </option>
+      </select>
+    </header>
+    <main class="flex flex-col items-center gap-4">
+      <section>
+        <p>
+          {{ $t("about") }}
+        </p>
+      </section>
 
-        <a href="https://github.com/SeryiBaran/tailvue-starter" target="_blank" class="btn">
-          <span class="icon icon-[mdi--github]" />Github
-        </a>
-      </main>
-    </div>
+      <a href="https://github.com/SeryiBaran/tailvue-starter" target="_blank" class="btn">
+        <span class="icon icon-[mdi--github]" />Github
+      </a>
+    </main>
   </div>
 </template>
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 export const messages = {
   ru: {
-    about: 'Шаблон проекта Vue с большим кол-вом штук. За подробностями - смотрите GitHub!',
+    about: 'Шаблон проекта Vue с большим кол-вом полезных штук. За подробностями - смотрите GitHub!',
   },
   en: {
-    about: 'Vue project template with a lot of things. For details - view GitHub!',
+    about: 'Vue project template with a lot of useful things. For details - view GitHub!',
   },
 }
